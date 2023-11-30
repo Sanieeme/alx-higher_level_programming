164a548377b7b24130020c36706274bbb383d988
sanieeme <shoneesani@gmail.com>
2023-11-29 18:20:08 -0800

    c islower

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for letters in range(97, 123):
+#for letters in range(97, 123):
+for letters in range(ord('a'), ord('z')+1):
     if letters in [101, 113]:
         continue
     print("{:c}". format(letters), end="")
