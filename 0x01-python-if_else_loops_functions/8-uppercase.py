433a9b6648efa351d3ab13dcd7e5d595b0e3e533
sanieeme <shoneesani@gmail.com>
2023-11-29 19:35:57 -0800

    last digit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letters in str:
+        letter = letters
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter = chr(ord(letters) - 32)
+        print ("{}".format(letters), end="")
+    print()
