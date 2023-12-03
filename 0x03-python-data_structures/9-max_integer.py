5e9d1926cec44c82667955c860d970a5995c583d
sanieeme <shoneesani@gmail.com>
2023-12-03 07:36:46 -0800

    max no. without max()

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if '' in my_list:
-        return None
+    large = my_list[0]
+    for n in my_list:
+        if n > large:
+            large = n
+    return(large)
 
