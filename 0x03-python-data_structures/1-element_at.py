8c6f9c0eb217bfc78ae5bb6e851d90f10743c9f1
sanieeme <shoneesani@gmail.com>
2023-12-02 22:31:52 -0800

    print list of integer

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    print("{}".format(my_list))
+    for idx in range(len(my_list)):
+        print("{}".format(idx))
+        if idx > 0:
+            return none
