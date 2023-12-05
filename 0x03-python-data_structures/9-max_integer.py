a2dbbf7ecf500d20a20d0bb4652cd6cc2717bcea
sanieeme <shoneesani@gmail.com>
2023-12-05 00:07:13 -0800

    max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if len(my_list) < 0:
         return (None)
-    large = my_list[0]
-    for n in my_list:
-        if n > large:
-            large = n
-    return (large)
+    my_list.sort()
+    return(my_list[-1])
