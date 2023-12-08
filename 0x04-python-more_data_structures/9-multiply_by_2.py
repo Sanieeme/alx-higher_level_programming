83944b29861c011c5e86fc1a69b3eb4cc9271566
Sanieeme <shoneesani@gmail.com>
2023-12-08 04:16:08 -0800

    dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    ;
+    dictionary = a_dictionary.copy()
+    lk = list(dictionary.keys())
+    for n in lk:
+        dictionary[n] *= 2
+        return (dictionary)
