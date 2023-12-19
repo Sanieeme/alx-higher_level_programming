fb8ce2fcbb72355befd12917afda69c0656b93e1
Sanieeme <shoneesani@gmail.com>
2023-12-19 05:24:20 -0800

    area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,7 +18,9 @@ class Square:
         if size < 0:
             raise ValueError('size must be >=0')
         self.__size = size
+
     def area(self):
+
         """return currebt square area
         """
         return (self.__size ** 2)
