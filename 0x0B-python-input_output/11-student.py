7cbf552911ed94beddd2face8cada9c09a51797d
Sanieeme <Shoneesani@gmail.com>
2024-01-29 13:16:51 -0800

    s

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,3 +23,9 @@ class Student:
             if key in attrs:
                 my_dict[key] = value
         return my_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
