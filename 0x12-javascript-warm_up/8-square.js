5b6dc3955acbbf19e09be4fc194c15e7ca5f391c
Sanieeme <Shoneesani@gmail.com>
2024-03-11 20:17:23 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = process.argv[2];
+if (isNaN(parseInt(size))) {
+	console.log("Missing size");
+} else {
+	const Size = parseInt(size);
+	for (let i = 0; i < Size; i++) {
+		let r = "";
+		for (let j = 0; j < Size; j++) {
+			r += "X";
+		}
+		console.log(r);
+	}
+}
