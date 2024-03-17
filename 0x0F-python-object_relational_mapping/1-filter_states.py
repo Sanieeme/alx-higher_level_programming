ff040bf63b6769fd6e67ac651a5b8365bb3d81e4
Sanieeme <shoneesani@gmail.com>
2024-03-17 11:09:27 -0700

    states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+d = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        d=sys.argv[3],
+        port=3306
+        )
+
+c = d.cursor()
+
+c.execute("SELECT * FROM states ORDER BY states.id")
+
+states = c.fetchall()
+
+for state in states:
+    print(state)
+
+    c.close()
+    d.close()
