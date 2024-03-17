e1e12e5dfa5b1907fbb88798f5383b691214667b
Sanieeme <shoneesani@gmail.com>
2024-03-17 11:27:55 -0700

    spqcing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,15 @@ import sys
 """script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
-    d = MySQLdb.connect(
+    db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
-            d=sys.argv[3],
+            db=sys.argv[3],
             port=3306
             )
 
-    c = d.cursor()
+    c = db.cursor()
 
     c.execute("SELECT * FROM states ORDER BY states.id")
 
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     for state in states:
         print(state)
 
-        c.close()
-        d.close()
+    c.close()
+    db.close()
