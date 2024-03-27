8eddd5427ad8463690e124c3686753cef79c2b93
Sanieeme <shoneesani@gmail.com>
2024-03-27 06:42:50 -0700

    is not a env bash??

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-# Bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response
+#!/usr/bin/bash
+# Bash script that takes in a URL, sends a request to that URL
 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
