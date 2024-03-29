5cecb5564bc665632adcd4e51e5f6aca55c443e2
Sanieeme <shoneesani@gmail.com>
2024-03-28 20:22:24 -0700

    email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Python script that takes in a URL and an email,
+# sends a POST request to the passed URL
+
+import urllib.request
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: python script.py <URL> <email>")
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+r = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(r) as response:
+    body = response.read().decode('utf-8')
+    print(body)                        
