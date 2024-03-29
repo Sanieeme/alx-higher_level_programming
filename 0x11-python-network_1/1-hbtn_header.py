f0611d4ef0f86d8f2c2a8b90a5a6e43f1f063e81
Sanieeme <shoneesani@gmail.com>
2024-03-28 20:11:46 -0700

    id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 # Python script that takes in a URL
 
+import urllib.request
+import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id header not found in the response.")
+except urllib.error.URLError as e:
+    print(e.reason)
