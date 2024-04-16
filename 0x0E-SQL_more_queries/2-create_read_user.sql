893d56f23ccea7875f4c96415dc138fb98370a01
Sanieeme <shoneesani@gmail.com>
2024-04-16 05:44:17 -0700

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+--  script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
