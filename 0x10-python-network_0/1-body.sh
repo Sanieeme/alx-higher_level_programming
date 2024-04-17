97141329428727947a4825667e4bf6cd1f9e7489
Sanieeme <Shoneesani@gmail.com>
2024-04-17 01:54:00 -0700

    s

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL 
-
+#!/bin/bash
+# Bash script that takes in a URL, sends a GET request to the URL
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && echo "Route 2"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# bash script that sends a DELETE request to the URL passed
 curl -X DELETE -sS $1
