e386c043637727f0ec0a46fd8fb93fbfc42ab1a1
sanieeme <Shoneesani@gmail.com>
2024-04-23 01:53:48 -0700

    resolve conflict

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -sI <URL> | awk '/Content-Length/ {print $2}' | tr -d '\r'
