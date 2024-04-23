ef1b6bb8c810011e9baaac5cc1616fd210ed82f4
sanieeme <shoneesani@gmail.com>
2024-04-23 11:26:55 -0700

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+request
+  .get(process.argv[2])
+  .on('error', function(err) {
+    console.error(err);
+  })
+  .pipe(fs.createWriteStream(process.argv[3]));
