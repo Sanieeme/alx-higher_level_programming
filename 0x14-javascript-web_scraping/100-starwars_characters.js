b063c61a81531900d593a7422e608f08e7fdb6b0
sanieeme <shoneesani@gmail.com>
2024-04-24 02:35:07 -0700

    characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request({
+  url: apiUrl,
+  method: 'GET',
+  strictSSL: false
+}, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+
+    const characters = movieData.characters;
+
+    const getCharacterData = (characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request({
+          url: characterUrl,
+          method: 'GET',
+          strictSSL: false
+	}, function (charError, charResponse, charBody) {
+          if (!charError && charResponse.statusCode === 200) {
+            const characterData = JSON.parse(charBody);
+            resolve(characterData);
+          } else {
+            reject(charError);
+          }
+        });
+      });
+    };
+    Promise.all(characters.map(getCharacterData))
+      .then((characterData) => {
+        characterData.forEach((character) => {
+          console.log(character.name);
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching character data:', error);
+      });
+  } else {
+    console.error('Error fetching movie data:', error);
+  }
+});
